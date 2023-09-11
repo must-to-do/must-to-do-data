c53436476c7b58e914220b44a231af03aff18054
jeounghan-yun <wjdgks7193@gmail.com>
2023-09-11 20:30:49 +0900

    data_crowing.py

--- data_crowing.py
+++ data_crowing.py
@@ -1,5 +1,3 @@
-import package
-
 headers = {'User-Agent' : 'Mozilla/5.0'}
 url = 'https://www.tripadvisor.co.kr/Attractions-g789433-Activities-Wonju_Gangwon_do.html'
 res = requests.get(url, header = headers)
